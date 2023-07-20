2351ec9c646b75cc8e90aad5f3eb6dcb082eb006
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2023-07-19 19:01:35 -0500

    springInitializr

---
+++ src/main/java/ec/edu/espe/arquitectura/examen2sebastianochoa/Examen2SebastianOchoaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examen2sebastianochoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2SebastianOchoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Examen2SebastianOchoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/espe/arquitectura/examen2sebastianochoa/Examen2SebastianOchoaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examen2sebastianochoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2SebastianOchoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
